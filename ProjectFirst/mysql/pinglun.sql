ce2956e7b5aa29152ce1517f903735a8c50f2bb1
maliang <924818949@qq.com>
2017-09-05 00:35:50 +0800

    将之前的jsp脚本全换成了jsp标签和EL表达式

---
+++ ProjectFirst/mysql/pinglun.sql
@@ -0,0 +1,8 @@
+INSERT INTO `pinglun` (`pid`, `pcontent`, `userid`, `byid`, `time`) VALUES (11, '我觉得很多地方都比美国强，当然总的看来还是差距很大的。', 8, 20, '2017-9-1 17:07:20');
+INSERT INTO `pinglun` (`pid`, `pcontent`, `userid`, `byid`, `time`) VALUES (12, '四大名著，其他的不晓得。。啊哈哈哈哈哈。。。。', 8, 22, '2017-9-1 17:08:18');
+INSERT INTO `pinglun` (`pid`, `pcontent`, `userid`, `byid`, `time`) VALUES (13, '你好，我是小明。', 8, 21, '2017-9-1 17:08:47');
+INSERT INTO `pinglun` (`pid`, `pcontent`, `userid`, `byid`, `time`) VALUES (14, '北方雪景很漂亮，1. 孤舟蓑笠翁，独钓寒江雪。 2. 云横秦岭家何在，雪拥蓝关马不前 3. 地白风色寒，雪花大入手 4. 夜深知雪重，时闻折竹声。 5. 北风吹雪四更初，嘉瑞天教及岁除。', 7, 23, '2017-9-1 17:09:43');
+INSERT INTO `pinglun` (`pid`, `pcontent`, `userid`, `byid`, `time`) VALUES (15, '北方雪景很漂亮，1. 孤舟蓑笠翁，独钓寒江雪。 2. 云横秦岭家何在，雪拥蓝关马不前 3. 地白风色寒，雪花大入手 4. 夜深知雪重，时闻折竹声。 5. 北风吹雪四更初，嘉瑞天教及岁除。', 7, 20, '2017-9-1 17:10:01');
+INSERT INTO `pinglun` (`pid`, `pcontent`, `userid`, `byid`, `time`) VALUES (16, '我可以给自己评论哦', 6, 24, '2017-9-1 17:15:41');
+INSERT INTO `pinglun` (`pid`, `pcontent`, `userid`, `byid`, `time`) VALUES (17, '小伙子，不错哦。', 6, 23, '2017-9-1 17:16:05');
+INSERT INTO `pinglun` (`pid`, `pcontent`, `userid`, `byid`, `time`) VALUES (18, '呀，，原来也可以给自己评论啊。', 7, 22, '2017-9-1 17:21:54');
---
+++ ProjectFirst/mysql/user.sql
@@ -0,0 +1,3 @@
+INSERT INTO `user` (`userid`, `name`, `password`, `email`, `family`, `intro`, `time`) VALUES (6, '古天乐', '123', 'yuggbbbujbj@ww.com', '北京', '1970年10月21日出生于中国香港，中国香港男演员、歌手。', '2017-9-1 16:47:16');
+INSERT INTO `user` (`userid`, `name`, `password`, `email`, `family`, `intro`, `time`) VALUES (7, '陈意涵', '123456', '927654672@qq.com', '中国台湾', '漂亮，美丽，人见人爱。', '2017-9-1 16:48:26');
+INSERT INTO `user` (`userid`, `name`, `password`, `email`, `family`, `intro`, `time`) VALUES (8, '马亮南生', '1234', '767676767@gmail.com', '四川成都', '克己复礼，帅气无比。', '2017-9-1 17:01:17');
